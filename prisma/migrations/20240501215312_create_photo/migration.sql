5f0d481b7c64402091a6bdf31a61cae3c1c70a95
Vincent Delsalle <vdelsalle@20minutes.fr>
2024-05-02 00:02:18 +0200

    create photo model

---
+++ prisma/migrations/20240501215312_create_photo/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Photo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `place` VARCHAR(191) NOT NULL,
+    `month` VARCHAR(191) NOT NULL,
+    `color` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
