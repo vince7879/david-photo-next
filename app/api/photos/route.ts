e8391a33e7ddddf1e9131e99b40701b6c6eb3903
Vincent Delsalle <vdelsalle@20minutes.fr>
2024-05-04 00:40:35 +0200

    build an API for creating photos

---
+++ app/api/photos/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from 'zod'
+import prisma from "@/prisma/client";
+
+const createPhotoSchema = z.object({
+    place: z.string().min(1).max(191),
+    month: z.string().min(1).max(191),
+    color: z.string().min(1).max(191)
+})
+
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    const validation = createPhotoSchema.safeParse(body)
+
+    if(!validation.success) 
+        return NextResponse.json(validation.error.errors, { status: 400 })
+
+    const newPhoto = await prisma.photo.create({
+        data: { place: body.place, month: body.month, color: body.color }
+    })
+
+    return NextResponse.json(newPhoto, { status: 201 })
+}
