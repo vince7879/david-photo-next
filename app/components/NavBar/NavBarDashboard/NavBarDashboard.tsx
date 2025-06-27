8aa317e145fbdcbad3e6ecac6240c5d13fee4b24
Vincent Delsalle <vdelsalle@20minutes.fr>
2025-06-27 12:12:16 +0200

    fix(NavBarDashboard): use IconButtons instead of Links

--- app/components/NavBar/NavBarDashboard/NavBarDashboard.tsx
+++ app/components/NavBar/NavBarDashboard/NavBarDashboard.tsx
@@ -3,29 +3,49 @@ import navBarStyles from "../NavBar.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import { signOut } from "next-auth/react";
-import { ExitIcon, ImageIcon, MoveIcon } from "@radix-ui/react-icons"
-
-// interface NavBarDashboardProps {
-//   currentColor: any;
-// }
+import { ExitIcon, ImageIcon, Pencil2Icon } from "@radix-ui/react-icons";
+import { IconButton } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 
 // @todo: move the navbar into the page where it's used if it's not used in multiple places
-const NavBarDashboard: React.FC = () => (
-  <nav className={`flex flex-col justify-between items-center ${navBarStyles.navBarDashboard}`}>
-  {/* mini-mondrian to go to the homepage */}
-  <Link href="/" className="mb-6">
-    <Image
-      src="/images/mondrian-mini.png"
-      alt="mondrian-mini"
-      width={50}
-      height={50}
-      priority={true}
-    />
-  </Link>
-  <Link href={"/dashboard/add-photo"} title="add a new photo" className="mb-3"><ImageIcon width="58" height="58"/></Link>
-  <Link href={"/dashboard/reorder-photos"} title="edit a gallery" className="mb-10"><MoveIcon width="58" height="58" /></Link>
-  <button title="sign out" onClick={() => signOut({ redirectTo: "/" })}><ExitIcon width="58" height="58" /></button>
-</nav>
-);
+const NavBarDashboard: React.FC = () => {
+  const router = useRouter();
+
+  return (
+    <nav
+      className={`flex flex-col justify-between items-center ${navBarStyles.navBarDashboard}`}
+    >
+      {/* mini-mondrian to go to the homepage */}
+      <Link href="/" className="mb-6">
+        <Image
+          src="/images/mondrian-mini.png"
+          alt="mondrian-mini"
+          width={50}
+          height={50}
+          priority={true}
+        />
+      </Link>
+      <IconButton
+        variant="ghost"
+        className={navBarStyles.navBarDashboard__toolButton}
+        onClick={() => router.push("/dashboard/add-photo")}
+        mb="3"
+      >
+        <ImageIcon width="58" height="58" />
+      </IconButton>
+      <IconButton
+        variant="ghost"
+        className={navBarStyles.navBarDashboard__toolButton}
+        onClick={() => router.push("/dashboard/reorder-photos")}
+        mb="9"
+      >
+        <Pencil2Icon width="58" height="58" viewBox="2 1 12 14" />
+      </IconButton>
+      <button title="sign out" onClick={() => signOut({ redirectTo: "/" })}>
+        <ExitIcon width="58" height="58" />
+      </button>
+    </nav>
+  );
+};
 
 export default NavBarDashboard;
